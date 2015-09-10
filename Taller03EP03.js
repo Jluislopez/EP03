10cfd2f0636bf544b795a852c80f73bf8c0fc394
Jluislopez <jllfigueredo.91@gmail.com>
2015-09-09 21:50:02 -0500

    Taller número 3
    Ejercicios de JS

---
+++ Taller03EP03.js
@@ -0,0 +1,64 @@
+
+var litros = 3.7854
+function conversion(galones){return galones*litros}
+
+var kelvin = 273.15
+function CelsiusAkelvin(Celsius){return Celsius+kelvin }
+
+var kelvin = 273.15
+function KelvinAcelsius(Kelvin){return Kelvin-kelvin}
+
+var conversion = 32
+function CelsiusAfarenheit(Celsius)
+{
+	return (Celsius*(9/5)+32)
+
+}
+
+
+var conversion = 32
+function CelsiusAfarenheit(Celsius)
+{
+	return (Celsius*(9/5)+32)
+
+}
+
+var conversion = 32
+
+function FarenheitAcelsius(Farennheit)
+{
+	return ((Farennheit-conversion)*(5/9))
+}
+
+var gravedad = 9.807
+
+function CalcularPesoLunar(Peso)
+{
+	return ((Peso)*(0.17*gravedad))
+}
+
+var pi = Math.pi
+
+function AreaCirculo(radio)
+{
+	return (pi*Math.sqrt(radio))
+}
+
+
+var pi = Math.pi
+
+function AreaCirculo(radio)
+{
+	return (Math.PI*Math.sqrt(radio))
+}
+
+
+function AreaTriangulo(Base, Altura)
+{
+	return ((Base*Altura)/2)
+}
+
+function AreaCuadrado(Lado)
+{
+	return Math.pow(Lado,2)
+}
